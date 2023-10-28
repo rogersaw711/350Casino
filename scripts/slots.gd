00762d3ab9934acc5a0a2b323289d8c7f3dad667
rogersaw711 <127700260+rogersaw711@users.noreply.github.com>
2023-10-27 22:02:11 -0400

    Add files via upload
    Quick last minute changes to remove unused assets.

--- scripts/slots.gd
+++ scripts/slots.gd
@@ -2,17 +2,10 @@ extends Node2D
 
 var iconTextures : Array
 var currentIconIndices : Array
-var leftSpinning : bool = false
-var middleSpinning: bool = false
-var rightSpinning: bool = false
-var spinDuration : float = 1.0
 
 var leftColumn
 var middleColumn
 var rightColumn
-var leftButton
-var middleButton
-var rightButton
 var startButton
 var coinCount
 var coinDisplay
@@ -21,9 +14,6 @@ func _ready():
 	leftColumn = $leftColumn
 	middleColumn = $middleColumn
 	rightColumn = $rightColumn
-	leftButton = $leftButton
-	middleButton = $middleButton
-	rightButton = $rightButton
 	startButton = $startButton
 	coinCount = get_node("/root/CoinCount")
 	coinDisplay = $coinDisplay
@@ -32,39 +22,27 @@ func _ready():
 		preload("res://assets/cherry.png"),
 		preload("res://assets/seven.png"),
 		preload("res://assets/bell.png"),
+		preload("res://assets/bar.png"),
 		# Add more icon textures here
 	]
 
 	currentIconIndices = [0, 0, 0]
 
-func _stopColumn(column_index):
-	var column = column_index
-	var index = column_index
-
-	if column_index == 0:
-		if leftSpinning:
-			leftColumn.texture = iconTextures[currentIconIndices[index]]
-			currentIconIndices[index] += 1
-
-		if currentIconIndices[index] >= iconTextures.size():
-			currentIconIndices[index] = 0
-	if column_index == 1:
-		if leftSpinning:
-			middleColumn.texture = iconTextures[currentIconIndices[index]]
-			currentIconIndices[index] += 1
-
-		if currentIconIndices[index] >= iconTextures.size():
-			currentIconIndices[index] = 0
-	if column_index == 2:
-		if leftSpinning:
-			rightColumn.texture = iconTextures[currentIconIndices[index]]
-			currentIconIndices[index] += 1
-
-		if currentIconIndices[index] >= iconTextures.size():
-			currentIconIndices[index] = 0
-	
-
 func _startSpinning():
+	"""
+	Starts the spinning of the slot columns, though this removes 50 chips from the count.
+	The user cannot play if they do not have 50 chips to bet.
+	This selects a random number for each of the three columns.
+	These random numbers are displayed as pre-determined icons, as shown in the iconTexture variable.
+	The numbers are then compared to determine what the payout is, which are listed here:
+		One cherry - 25 chips
+		Two Cherry - 50 Chips
+		Three cherry - 75 Chips
+		Two Bells - 100 Chips
+		Three Bells - 150 Chips
+		Three Sevens - 200 Chips
+		Bars are worth nothing, regardless of how many there are.
+	"""
 	leftSpinning = true
 	middleSpinning = true
 	rightSpinning = true
@@ -72,17 +50,22 @@ func _startSpinning():
 		return
 	coinCount.addCoins(-50)
 	# Start the Timer to control the animation
-	$SpinningTimer.start()
 	# Initialize a list to store the selected icon index for each column
 	var selectedIcons = []
 	var cherryCount = 0
+	var bellCount = 0
+	var sevenCount = 0
 
 	for i in range(3):
 		# Randomly select an icon index for each column
 		var iconIndex = randi() % iconTextures.size()
 		selectedIcons.append(iconIndex)
 		if iconIndex == 0:
-				cherryCount += 1
+			cherryCount += 1
+		if iconIndex == 1:
+			sevenCount += 1
+		if iconIndex == 2:
+			bellCount += 1
 		# Initialize the column with the selected icon
 		match i:
 			0:
@@ -93,9 +76,11 @@ func _startSpinning():
 				rightColumn.texture = iconTextures[iconIndex]
 
 	# Pass the selected icon indices to the Timer's `userdata` property
-	if selectedIcons == [1,1,1]:
+	if bellCount == 2: 
 		coinCount.addCoins(100)
-	if selectedIcons == [2,2,2]:
+	if bellCount == 3:
+		coinCount.addCoins(150)
+	if sevenCount == 3:
 		coinCount.addCoins(200)
 	if cherryCount == 1:
 		coinCount.addCoins(25)
@@ -105,43 +90,3 @@ func _startSpinning():
 		coinCount.addCoins(75)
 	
 	coinDisplay.text = "Coin Count: " + str(coinCount.coinCount)
-	$SpinningTimer.set("userdata", selectedIcons)
-
-func _stopSpinning():
-	$SpinningTimer.stop()
-	#spinning = false
-
-func _on_Timer_timeout():
-	for i in range(3):
-		var column
-		var index
-
-		match i:
-			0:
-				column = leftColumn
-				index = 0
-			1:
-				column = middleColumn
-				index = 1
-			2:
-				column = rightColumn
-				index = 2
-
-		column.texture = iconTextures[currentIconIndices[index]]
-		currentIconIndices[index] += 1
-
-		if currentIconIndices[index] >= iconTextures.size():
-			currentIconIndices[index] = 0
-
-
-
-func _on_left_button_pressed():
-	_stopColumn(0)
-
-
-func _on_middle_button_pressed():
-	_stopColumn(1)
-
-
-func _on_right_button_pressed():
-	_stopColumn(2)
